4d143b688d135050e11582bb44bcddf9d11db8d7
zhouhaoyu <67226385+zhouhaoyiu@users.noreply.github.com>
2021-08-13 13:14:05 +0800

    add

---
+++ plugins/md-to-html-plugin/README.md
@@ -0,0 +1,9 @@
+# 1、markdown文件
+
+# 2、template模板 HTML
+
+# 3、markdown ->HTML标签
+
+# 4、HTML标签替换掉template内部<!-- inner -->
+
+# 5、webpack打包
---
+++ plugins/md-to-html-plugin/index.js
@@ -0,0 +1,38 @@
+const { readFileSync } = require('fs')
+const { resolve } = require('path')
+const { compilerHTML } = require('./compiler')
+const INNER_MARK = '<!-- inner -->'
+
+class MdToHtmlPlugin {
+  constructor({ template, filename }) {
+    if (!template) {
+      throw new Error('The config for "templete" must be configured')
+    }
+    this.template = template
+    this.filename = filename ? filename : 'md.html'
+  }
+
+  apply(compiler) {
+    compiler.hooks.emit.tap('md-to-html-plugin', (compilation) => {
+      const _assets = compilation.assets
+      const _mdContent = readFileSync(this.template, 'utf8')
+      const _templateHTML = readFileSync(resolve(__dirname, 'template.html'), 'utf8')
+      const _mdContentArr = _mdContent.split('\r\n')
+      const _htmlStr = compilerHTML(_mdContentArr)
+      // console.log(_htmlStr)
+        const _finalHTML = _templateHTML.replace(INNER_MARK, _htmlStr)
+
+        _assets[this.filename] = {
+          source() {
+            return _finalHTML
+          },
+
+          size() {
+            return _finalHTML.length
+          }
+        }
+    })
+  }
+}
+
+module.exports = MdToHtmlPlugin
